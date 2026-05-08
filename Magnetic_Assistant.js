e43f42b42f4a4a90d1b07fbf17bd3cd2fff029ff
ZiPenOk <freetuchuang@gmail.com>
2026-05-08 15:43:38 +0800

    up

--- Magnetic_Assistant.js
+++ Magnetic_Assistant.js
@@ -2,7 +2,7 @@
 // @name         磁力&电驴链接助手
 // @namespace    https://github.com/ZiPenOk
 // @version      3.4.2
-// @description  点击按钮显示绿色勾（验车按钮除外），支持复制（自动精简链接，保留xt和dn并提取番号）、推送到qB/115，新增磁力信息验车功能，截图轮播。现增强：支持FTP链接、纯哈希值转磁力、文本链接着色。
+// @description  点击按钮显示绿色勾（验车按钮除外），支持复制（自动精简链接，保留xt和dn并提取番号）、推送到qB/115，新增磁力信息验车功能，截图轮播。
 // @icon         https://uxwing.com/wp-content/themes/uxwing/download/seo-marketing/magnet-magnetic-icon.png
 // @match        *://*/*
 // @grant        GM_xmlhttpRequest
