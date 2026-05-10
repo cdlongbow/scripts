5c89cc4c38668b464d3f3be15720e41becf73edc
ZiPenOk <freetuchuang@gmail.com>
2026-05-10 16:36:31 +0800

    fix

--- cilijia.js
+++ cilijia.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         磁力家磁力链提取
 // @namespace    https://github.com/ZiPenOk
-// @version      1.1
+// @version      1.2
 // @description  磁力家搜索结果加载后为每个资源添加磁力链接和复制按钮,可能需要手动刷新一次页面
 // @author       ZiPenOk
 // @match        https://cilijia.net/search*
-// @match        https://taocili.net/search*
+// @match        https://taocili.com/search*
 // @grant        none
 // @run-at       document-idle
 // @supportURL   https://github.com/ZiPenOk/scripts/issues
 // @homepageURL  https://github.com/ZiPenOk/scripts
-// @icon         https://cilijia.net/favicon.ico
+// @icon         https://taocili.com/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/ZiPenOk/scripts/main/cilijia.js
 // @downloadURL  https://raw.githubusercontent.com/ZiPenOk/scripts/main/cilijia.js
 // ==/UserScript==
